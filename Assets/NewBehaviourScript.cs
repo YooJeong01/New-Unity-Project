e34ba82a5cc6ed5e2c1e627c4fc72dc21b37ba33
y2j0h <ujjh77@naver.com>
2021-10-09 00:17:23 +0900

    .

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    int sceneIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class NewBehaviourScript : MonoBehaviour
     public void OnClickNewGame()
     {
         Debug.Log("시작하기");
+        SceneManager.LoadScene(sceneIndex + 1);
     }
 
     public void OnClickLoad()
@@ -29,6 +32,7 @@ public class NewBehaviourScript : MonoBehaviour
     public void OnClickInfo()
     {
         Debug.Log("개발자 정보");
+        SceneManager.LoadScene(sceneIndex + 2);
     }
 
 }
