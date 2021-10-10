95fe4a7b10a4de7656bd9b44808dae019a8c80af
kkng932 <88977637+kkng932@users.noreply.github.com>
2021-10-10 16:57:39 +0900

    timer add

---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerTxt;
+    public float time = 3f;
+    private float selectCountdown;
+
+    void Start()
+    {
+        selectCountdown = time;
+    }
+
+    void Update()
+    {
+        if (Mathf.Floor(selectCountdown) <= 0)
+        {
+            // Count 0일때 동작할 함수 삽입
+        }
+        else
+        {
+            selectCountdown -= Time.deltaTime;
+            timerTxt.text = Mathf.Floor(selectCountdown).ToString();
+        }
+    }
+}
