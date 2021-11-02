b0712b2ce7b9e36f60035584b962d1de66beaf2f
y2j0h <ujjh77@naver.com>
2021-11-02 21:11:16 +0900

    11.2

---
+++ Assets/Scripts/DataMgr.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Character
+{
+    Hensel,Gretel
+}
+
+public class DataMgr : MonoBehaviour
+{
+    public static DataMgr instance;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != null) return;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public Character currentCharacter;
+
+
+
+}
---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    int sceneIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickChoice()
+    {
+        Debug.Log("시작하기");
+        SceneManager.LoadScene("Scene_Select");
+    }
+
+    public void OnClickPlay()
+    {
+        Debug.Log("캐릭터 선택 완료 및 게임 시작");
+        SceneManager.LoadScene("Scene_Start");
+    }
+
+    public void OnClickLoad()
+    {
+        Debug.Log("이어하기");
+    }
+
+    public void OnClickInfo()
+    {
+        Debug.Log("개발자 정보");
+        SceneManager.LoadScene("Scene_imformation");
+    }
+
+    public void OnClickInfoBack()
+    {
+        Debug.Log("돌아가기");
+        SceneManager.LoadScene("Scene_MainMenu");
+    }
+
+}
