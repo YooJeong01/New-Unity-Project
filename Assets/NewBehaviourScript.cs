0f39e5502838a313acce65e3c5c4b62d4356b56b
kkng932 <88977637+kkng932@users.noreply.github.com>
2021-11-04 02:05:15 +0900

    추가한거만 커밋

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+        Debug.Log("시작하기");
+    }
+
+    public void OnClickLoad()
+    {
+        Debug.Log("이어하기");
+    }
+
+    public void OnClickInfo()
+    {
+        Debug.Log("개발자 정보");
+    }
+
+}
---
+++ Assets/itemscrollerrr.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemscrollerrr : MonoBehaviour
+{
+    public SpriteRenderer[] tiles;
+    public Sprite[] groundImg;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        temp = tiles[0];
+    }
+    SpriteRenderer temp;
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            if (-5 >= tiles[i].transform.position.x)
+            {
+
+                for (int q = 0; q < tiles.Length; q++)
+                {
+                    if (temp.transform.position.x < tiles[q].transform.position.x)
+                        temp = tiles[q];
+                }
+                tiles[i].transform.position = new Vector2(temp.transform.position.x + 13, -4.5f);
+                tiles[i].sprite = groundImg[Random.Range(0, groundImg.Length)];
+            }
+        }
+
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tiles[i].transform.Translate(new Vector2(-1, 0) * Time.deltaTime * speed);
+        }
+    }
+}
